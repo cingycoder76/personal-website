99c1620c9b84cb5f2ed00530fdd6da20a532a2da
Abhaya Bikram Shahi <abhayabikramshahiofficial@gmail.com>
2024-12-04 22:18:31 +0545

    Big changes

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ document.getElementById("call").addEventListener("click", () => {
 document.getElementById("school").addEventListener("click", () => {
     window.location.href = "https://www.collegenp.com/school/badimalika-secondary-school-kalikot";
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const preloader = document.getElementById("preloader");
+    const hasSeenPreloader = localStorage.getItem("hasSeenPreloader");
+
+    if (!hasSeenPreloader) {
+        document.body.classList.add("loading");
+        setTimeout(() => {
+            preloader.style.display = "none";
+            document.body.classList.remove("loading");
+            localStorage.setItem("hasSeenPreloader", "true");
+        }, 3000);
+    } else {
+        preloader.style.display = "none";
+    }
+});
